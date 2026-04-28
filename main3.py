b2ff1081fe8e08464fd9d02eb7ce3f6517555f20
aestheticqueen001 <parnikapitrubhakta@gmail.com>
2026-04-28 16:25:01 +0530

    Add files via upload

---
+++ main3.py
@@ -0,0 +1,7 @@
+lst = ['Strawberry','Mango','Pineapple','Orange','Grapes']
+
+print("Length of list:",len(lst))
+print("First Element:",lst[0])
+print("Last Element:",lst[-1])
+
+lst.append('Papaya')
