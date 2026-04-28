64984cf7fef6fb11de4b82eb25e0026b39248253
aestheticqueen001 <parnikapitrubhakta@gmail.com>
2026-04-28 17:12:05 +0530

    Add files via upload

---
+++ main4.py
@@ -0,0 +1,8 @@
+number = int(input("Enter Number to Check :"))
+print("Number to be checked :", number)
+
+if number >0 :
+    print("This is an natural number")
+
+else:
+     print("This is an whole number")
